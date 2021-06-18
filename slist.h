686f51561f01c32893e6fe0099b9a030a4f900d6
Martin Goñi <46424341+Tinch334@users.noreply.github.com>
2021-06-18 09:45:24 -0300

    Finished converting library to use given struct

--- slist.h
+++ slist.h
@@ -10,15 +10,19 @@
 //Ahora, lo que estamos haciendo es llamar con el identificador 
 //FuncionVisitante a cualquier función que tome un entero y tenga tipo
 //de retorno void 
-typedef void (*FuncionVisitante) (int dato);
-
-typedef int (*FuncionComparacion) (int dato1, int dato2);
-
+typedef struct {
+    char *nombre, *direccion, *telefono, *mail, *aliasTelegram, *usuarioInstagram;
+} Contacto;
 
 typedef struct _SNodo {
-  int dato;
+  Contacto dato;
   struct _SNodo *sig;
 } SNodo;
+
+typedef void (*FuncionVisitante) (Contacto dato);
+
+typedef int (*FuncionComparacion) (Contacto dato1, Contacto dato2);
+
 
 //Cuando usamos SList estamos representando un SNodo*
 //es decir, usar SList sería lo mismo que usar: struct _SNodo *
@@ -40,14 +44,14 @@ void slist_destruir(SList lista);
 int slist_vacia(SList lista);
 
 /**
- * Agrega un elemento al final de la lista.
+ * Agrega un elemento al final de la lista. Se le pasan los argumentos de manera individual para evitar tener que crear una struct.
  */
-SList slist_agregar_final(SList lista, int dato);
+SList slist_agregar_final(SList lista, char *nombre, char *direccion, char *telefono, char *mail, char *aliasTelegram, char *usuarioInstagram);
 
 /**
- * Agrega un elemento al inicio de la lista.
+ * Agrega un elemento al inicio de la lista. Se le pasan los argumentos de manera individual para evitar tener que crear una struct.
  */
-SList slist_agregar_inicio(SList lista, int dato);
+SList slist_agregar_inicio(SList lista, char *nombre, char *direccion, char *telefono, char *mail, char *aliasTelegram, char *usuarioInstagram);
 
 /**
  * Recorrido de la lista, utilizando la funcion pasada.
@@ -59,48 +63,18 @@ void slist_recorrer(SList lista, FuncionVisitante visit);
  */
 int slist_longitud(SList lista);
 
-/**
- * Concatena la primera lista con la segunda lista, almacenando el resultado en la primera.
- */
-SList slist_concatenar(SList lista1, SList lista2);
-
-/**
- * Inserta un elemento en la posicion dada. Devuelve 0 si el elemento se inserto de manera correcta y 1 si ocurrio un error. Tomar en cuenta que posicion empieza a contar desde 0.
- */
-int slist_insertar(SList *lista, int posicion, int dato);
-
 /*
  * Borra de la lista un elemento en la posicion dada. Devuelve 0 si el elemento se borro de manera correcta y 1 si ocurrio un error. Tomar en cuenta que la posicion empieza a contar desde 0.
  */
 int slist_eliminar(SList *lista, int posicion);
 
 /*
- * Determina si un elemento esta en la lista. Devuelve 0 si no esta y 1 si esta.
+ * Determina si un elemento esta en la lista. Devuelve 0 si no esta y 1 si esta. Se le pasa solo el nombre del elemento, no toda la struct.
  */
-int slist_contiene(SList lista, int dato);
+int slist_contiene(SList lista, char *nombre);
 
 /*
- * Si un elemento esta en la lista devuelve su indice, empezando desde 0. Si no esta devuelve -1.
+ * Si un elemento esta en la lista devuelve su indice, empezando desde 0. Si no esta devuelve -1. Se le pasa solo el nombre del elemento, no toda la struct.
  */
-int slist_indice(SList lista, int dato);
-
-/*
- * Devuelve una SList con los elementos comunes a ambas listas, si no hay elementos comunes devuelve NULL.
- */
-SList slist_intersecar(SList lista1, SList lista2);
-
-/*
- * Devuelve una SList con los elementos comunes a ambas listas, si no hay elementos comunes devuelve NULL. Para determinar que elementos son iguales/comunes usa la funcion pasada, que toma los dos datos y devuelve 1 si son iguales y 0 si no. Finalmente "ambos", si es 1 ambos valores deben ser almacenados, es decir el de la lista1 y el de la lista2, es util cuando el concepto de igualdad no implica igualdad tradicional.
- */
-SList slist_intersecar_custom(SList lista1, SList lista2, FuncionComparacion comp, int ambos);
-
-/*
- * Devuleve el reverso de la lista dada.
- */
-SList slist_reverso(SList lista);
-
-/*
- * Devuelve una lista con los elementos de las listas pasadas intercalados. Por ejemplo dadas [1, 2, 3, 4] y [5, 6] devuelve [1, 5, 2, 6, 3, 4].
- */
-SList slist_intercalar(SList lista1, SList lista2);
+int slist_indice(SList lista, char *nombre);
 #endif /* __SLIST_H__ */
