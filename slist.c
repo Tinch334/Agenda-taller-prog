0cfd7798ca2ea2a4efedd72093537a2ccfdf2f81
maph <71228707+maph420@users.noreply.github.com>
2021-08-19 20:55:24 -0300

    fixed -gstabs/-Wall compiling warnings

--- slist.c
+++ slist.c
@@ -1,6 +1,7 @@
 #include "slist.h"
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /*
  * NOTA: Para usar funciones con punteros dobles(**) hay que usar el * para desreferenciar adentro de un parentesis. De lo contrario el operador "->" toma precedencia y la expresion no funciona. Recordar ademas que "SList" ya es un puntero(Ver su declaracion "slist.h"). Ejemplo:
@@ -23,15 +24,15 @@ void borrar_struct(Contacto structABorrar) {
     free(structABorrar.usuarioInstagram);
 }
 
-//Esta función destruye todos los nodos de la lista haciendo que 
-//liberemos la memoria del nodo usando free y, pasando al siguiente
-//hasta llegar a NULL
+/*Esta función destruye todos los nodos de la lista haciendo que 
+liberemos la memoria del nodo usando free y, pasando al siguiente
+hasta llegar a NULL*/
 void slist_destruir(SList lista) {
   SNodo *nodoAEliminar;
 
   while (lista != NULL) {
     nodoAEliminar = lista;
-    //Destruye todas las strings de la struct y su espacio asociado en memoria.
+    /*Destruye todas las strings de la struct y su espacio asociado en memoria.*/
     borrar_struct(nodoAEliminar->contacto);
 
     lista = lista->sig;
@@ -47,7 +48,7 @@ int slist_vacia(SList lista) {
 SList slist_agregar_final(SList lista, Contacto dato) {
   SNodo *nuevoNodo = malloc(sizeof(SNodo));
   //no sería conveniente tener una función que clone un contacto y retorne dicho contado?
-  //"strdup" es una funcion de "string.h" que copia la string provista devolviendo un puntero a la misma, evitando tener que asignar memoria manualmente.
+  /* "strdup" es una funcion de "string.h" que copia la string provista devolviendo un puntero a la misma, evitando tener que asignar memoria manualmente. */
   nuevoNodo->contacto.nombre = strdup(dato.nombre);
   nuevoNodo->contacto.direccion = strdup(dato.direccion);
   nuevoNodo->contacto.telefono = strdup(dato.telefono);
@@ -84,58 +85,47 @@ int slist_longitud(SList lista) {
     return longitud;
 }
 
-//Borrar el primer o ultimo elemento no funciona con una lista de un solo elemento.
 int slist_eliminar(SList *lista, int posicion) {
-     //Tomo en cuenta que se pueden borrar elementos al principio y al final de la lista.
+     /*Tomo en cuenta que se pueden borrar elementos al principio y al final de la lista.*/
     if (posicion > slist_longitud(*lista))
         return 1;
     else if (posicion == 0) {
-        //Guardo el primer nodo. Reasigno al segundo nodo como primer elemento de la lista y borro el primero.
+        /*Guardo el primer nodo. Reasigno al segundo nodo como primer elemento de la lista y borro el primero.*/
         SList nodo = *lista;
         *lista = (*lista)->sig;
-
         borrar_struct(nodo->contacto);
         free(nodo);
-        //no era mejor poner un return acá?
-    }
-    //por qué hago el caso último separado de los otros casos? borrar el último es lo mismo que borrar cualquier nodo intermedio.
-    //su siguiente es NULL.
-    else if (posicion == slist_longitud(*lista)) {
-        SList nodo = *lista;
-
-        //Va al penultimo elemento de la lista.
-        for (int i = 0; i < slist_longitud(*lista) - 2; i++, nodo = nodo->sig);
-
-        //Usando el penultimo nodo elimina el ultimo y hace que el proximo elemento sea "NULL".
-        borrar_struct(nodo->contacto);
-        free(nodo->sig);
-        nodo->sig = NULL;
+        return 1;
     }
     else {
-        //Para borrar un elemento en medio de la lista "sig" del nodo anterior a su posicion debe pasar a apuntar al proximo nodo.
+        /*Para borrar un elemento en medio de la lista "sig" del nodo anterior a su posicion debe pasar a apuntar al proximo nodo.*/
         SList anterior = *lista;
 
         for (int i = 0; i < posicion - 1; i++, anterior = anterior->sig);
 
-        //Borro el nodo deseado y asigno el "sig" del anterior al posterior.
+        /*Borro el nodo deseado y asigno el "sig" del anterior al posterior.*/
         SList posterior = anterior->sig->sig;
         borrar_struct(anterior->sig->contacto);
         free(anterior->sig);
         anterior->sig = posterior;
     }
-
     return 0;
 }
 
-//Recorro la lista buscando donde está el nombre a eliminar y luego llamo a una función que se desplaza hasta esa posición. 
-//No se podía hacer todo junto?
 int slist_indice(SList lista, char *nombre) {
     SList nodo = lista;
-    int ret = -1;
+    char aux[255];
 
-    for (int i = 0; nodo != NULL && strcmp(nodo->contacto.nombre, nombre) == 0; nodo = nodo->sig, i++)
-        if (strcmp(nodo->contacto.nombre, nombre) == 0)
-            ret = i;
+    if (lista == NULL)
+        return -1;
 
-    return ret;
+    for (int i = 0; nodo != NULL; nodo = nodo->sig, i++) {
+      strcpy(aux, nodo->contacto.nombre);
+      for (int i = 0; i < strlen(aux); i++)
+        aux[i] = tolower(aux[i]);
+
+      if (!strcmp(aux, nombre))
+            return i;
+  }
+    return -1;
 }
