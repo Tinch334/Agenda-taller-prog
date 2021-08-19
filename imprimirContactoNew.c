6a9ad00f9d226a55c6af69eeac0a808252efb47b
maph <71228707+maph420@users.noreply.github.com>
2021-08-19 20:55:47 -0300

    Delete imprimirContactoNew.c

--- imprimirContactoNew.c
+++
@@ -1,4 +0,0 @@
-static void imprimirContacto(Contacto contacto) {
-    printf("\n%-10s \t %-s \t %-7s \t %-25s \t %-15s \t %-10s \t \n","Nombre","Direccion","Telefono","Mail","Telegram","Instagram");    
-    printf("%-10s \t %-s \t %-7s \t %-25s \t %-15s \t %-10s \t \n",contacto.nombre,contacto.direccion,contacto.telefono,contacto.mail,contacto.aliasTelegram,contacto.usuarioInstagram);
-}
